8ccfffbcd10a21c4273e8e6ea5bbff6576373b76
rcrdrobson <rcrdrobson@gmail.com>
2016-05-24 13:58:15 -0300

    Downgrade Perigoso, Cuidado com as Configurações - JUNGLE

--- test/cucumber/steps/PoliticoSteps.groovy
+++ test/cucumber/steps/PoliticoSteps.groovy
@@ -1,5 +1,5 @@
+import obraspublicas.*
 import steps.TestDataAndOperations
-
 import static cucumber.api.groovy.EN.*
 
 
@@ -9,21 +9,59 @@ import static cucumber.api.groovy.EN.*
 //When eu tentar cadastrar um político com o nome “Eduardo” e CPF “01234567890”
 //Then o sistema irá cadastrar o político com o nome “Eduardo” e CPF “01234567890”
 
-Given(~'^o sistema não tem um político com o CPF "([^"]*)"$'){
-    String cpf ->
-        Politico politico = Politico.findByCPF(cpf)
-        assert politico==null
-}
-
-When (~'^eu tentar cadastrar um politico com o CPF "([^"]*)"$'){
-    String cpf ->
-        TestDataAndOperations.createPolitico(cpf)
-}
-
-Then(~'^o sistema irá cadastrar o politico de CPF "([^"]*)"$'){
+Given(~'^o sistema não tem um político com o nome "([^"]*)" e CPF "([^"]*)"$'){
     String politicoName, cpf ->
-        Politico politico = Politico.findByCPF(cpf)
-        assert TestDataAndOperations.politicoCompatibleTo(politico, cpf)
+    Politico politico = Politico.findByCpfAndName(cpf,politicoName)
+    assert politico==null
+}
+
+When (~'^eu tentar cadastrar um politico com o nome "([^"]*)" e CPF "([^"]*)"$'){
+    String politicoName,cpf ->
+    TestDataAndOperations.createPolitico(politicoName,cpf)
+}
+
+Then(~'^o sistema irá cadastrar o politico de nome "([^"]*)" e CPF "([^"]*)"$'){
+    String politicoName, cpf ->
+        Politico politico = Politico.findByName(politicoName)
+        assert TestDataAndOperations.compatibleTo(politico, politicoName,cpf)
+}
+
+//Scenario: Atualizar político
+//Given que exista um politico com nome “Eduardo” e CPF "01234567890”
+//When eu tentar atualizar os dados do político  com nome “Eduardo” e CPF "01234567890”
+//Then o sistema irá atualizar o político
+Given(~'^que existe um politico no sistema chamado"([^"]*)" e CPF "([^"]*)"$'){
+    String politicoName, cpf ->
+    Politico politico = Politico.findByCpfAndName(cpf, politicoName)
+    assert politico != null
+}
+
+When (~'^eu tento atualizar os dados do polico com o nome"([^"]*)" e CPF "([^"]*)"$') {
+    String politicoName, cpf ->                        //tem que verificar
+     TestDataAndOperations.atualizarPolitico(politicoName,cpf)//tem que verificar
+}
+
+Then(~'^o sistema irá atualizar o politico de nome "([^"]*)" e CPF "([^"]*)"$') {
+}
+//Scenario: Remover político existente
+//Given o sistema tem um político com nome “Eduardo” e CPF "01234567890”
+//When eu tentar remover o político com o nome “Eduardo” e CPF “01234567890”
+//Then o sistema ira mostrar a mensagem politico "Eduardo" de CPF “01234567890” removido com sucesso
+Given(~'^o sistema tem um político chamado "([^"]*)" e CPF "([^"]*)"$'){
+    String politicoName, cpf ->
+    Politico politico = Politico.findByCpfAndName(cpf, politicoName)
+    assert politico != null
+}
+
+When (~'^eu tento remover o polico com o nome"([^"]*)" e CPF "([^"]*)"$') {
+    String politicoName, cpf ->                        //tem que verificar
+     TestDataAndOperations.deletarPolitico(politicoName,cpf)//tem que verificar
+}
+
+Then(~'^o sistema irá remover o politico de nome "([^"]*)" e CPF "([^"]*)"$'){
+    String politicoName, cpf ->
+        Politico politico = Politico.findByName(politicoName)
+        assert TestDataAndOperations.compatibleTo(politico, politicoName,cpf)
 }
 
 //Scenario: Adicionar político existente
@@ -31,19 +69,19 @@ Then(~'^o sistema irá cadastrar o politico de CPF "([^"]*)"$'){
 //When eu tentar cadastrar um político com o nome “Eduardo” e CPF “01234567890”
 //Then o sistema não irá cadastrar o político
 
-Given(~'^o sistema tem um político de CPF "([^"]*)"$'){
-    String cpf ->
-        Politico politico = Politico.findByCPF(cpf)
-        assert politico != null
+Given(~'^o sistema tem um político de nome "([^"]*)" e CPF "([^"]*)"$'){
+    String politicoName, cpf ->
+    Politico politico = Politico.findByCpfAndName(cpf,politicoName)
+    assert politico != null
 }
 
-When (~'^ao tentar cadastrar um político com o CPF "([^"]*)"$'){
-    String cpf ->
-        TestDataAndOperations.createPolitico(cpf)
+When (~'^ao tentar cadastrar um político com o nome "([^"]*)" e CPF "([^"]*)"$'){
+    String politicoName, cpf ->                        //tem que verificar
+    TestDataAndOperations.createPolitico(politicoName,cpf)//tem que verificar
 }
 
-Then(~'^o sistema não irá cadastrar o político de CPF "([^"]*)"$') {
-    String cpf ->
-        politicos = Politico.findAllByCPF(cpf)
-        assert politicos.size() == 1
+Then(~'^o sistema não irá cadastrar o político de nome "([^"]*)" e CPF "([^"]*)"$') {
+    String politicoName,cpf ->
+    politicos = Politico.findAllByCpfAndName(cpf,politicoName)
+    assert politicos.size() == 1
 }
