1783e3eac651b6fd6e4221ad64e55a74c2aa6a09
Ricardo <rcrdrobson@gmail.com>
2016-06-15 21:06:46 -0300

    Tasks - Testes Adicionar Politico existente e não existente: #121635329 #121635251

--- test/cucumber/steps/PoliticoSteps.groovy
+++ test/cucumber/steps/PoliticoSteps.groovy
@@ -9,21 +9,21 @@ import static cucumber.api.groovy.EN.*
 //When eu tentar cadastrar um político com o nome “Eduardo” e CPF “01234567890”
 //Then o sistema irá cadastrar o político com o nome “Eduardo” e CPF “01234567890”
 
-Given(~'^o sistema não tem um político com o nome "([^"]*)" e CPF "([^"]*)"$'){
-    String politicoName, cpf ->
-    Politico politico = Politico.findByCpfAndName(cpf,politicoName)
-    assert politico==null
+Given(~'^que o sistema nao tem um politico com CPF "([^"]*)"$'){
+    String cpf ->
+        Politico politico = Politico.findByCpf(cpf)
+        assert politico == null
 }
 
-When (~'^eu tentar cadastrar um politico com o nome "([^"]*)" e CPF "([^"]*)"$'){
-    String politicoName,cpf ->
-    TestDataAndOperations.createPolitico(politicoName,cpf)
+When(~'^eu tentar cadastrar um politico com CPF "([^"]*)"$'){
+    String cpf ->
+        TestDataAndOperations.createPolitico(cpf)
 }
 
-Then(~'^o sistema irá cadastrar o politico de nome "([^"]*)" e CPF "([^"]*)"$'){
-    String politicoName, cpf ->
-        Politico politico = Politico.findByName(politicoName)
-        assert TestDataAndOperations.compatibleTo(politico, politicoName,cpf)
+Then(~'^o sistema ira cadastrar o politico de CPF "([^"]*)"$'){
+    String cpf ->
+        Politico politico = Politico.findByCpf(cpf)
+        assert TestDataAndOperations.politicoCompatibleTo(politico, cpf)
 }
 
 //Scenario: Atualizar político
@@ -32,7 +32,7 @@ Then(~'^o sistema irá cadastrar o politico de nome "([^"]*)" e CPF "([^"]*)"$')
 //Then o sistema irá atualizar o político
 Given(~'^que existe um politico no sistema chamado"([^"]*)" e CPF "([^"]*)"$'){
     String politicoName, cpf ->
-    Politico politico = Politico.findByCpfAndName(cpf, politicoName)
+    Politico politico = Politico.findByCpfAndNome(cpf, politicoName)
     assert politico != null
 }
 
@@ -43,6 +43,9 @@ When (~'^eu tento atualizar os dados do polico com o nome"([^"]*)" e CPF "([^"]*
 
 Then(~'^o sistema irá atualizar o politico de nome "([^"]*)" e CPF "([^"]*)"$') {
 }
+
+
+
 //Scenario: Remover político existente
 //Given o sistema tem um político com nome “Eduardo” e CPF "01234567890”
 //When eu tentar remover o político com o nome “Eduardo” e CPF “01234567890”
@@ -53,7 +56,7 @@ Given(~'^o sistema tem um político chamado "([^"]*)" e CPF "([^"]*)"$'){
     assert politico != null
 }
 
-When (~'^eu tento remover o polico com o nome"([^"]*)" e CPF "([^"]*)"$') {
+When (~'^eu tento remover o politico com o nome"([^"]*)" e CPF "([^"]*)"$') {
     String politicoName, cpf ->                        //tem que verificar
      TestDataAndOperations.deletarPolitico(politicoName,cpf)//tem que verificar
 }
@@ -61,7 +64,7 @@ When (~'^eu tento remover o polico com o nome"([^"]*)" e CPF "([^"]*)"$') {
 Then(~'^o sistema irá remover o politico de nome "([^"]*)" e CPF "([^"]*)"$'){
     String politicoName, cpf ->
         Politico politico = Politico.findByName(politicoName)
-        assert TestDataAndOperations.compatibleTo(politico, politicoName,cpf)
+        assert TestDataAndOperations.politicoCompatibleTo(politico, cpf)
 }
 
 //Scenario: Adicionar político existente
@@ -69,19 +72,15 @@ Then(~'^o sistema irá remover o politico de nome "([^"]*)" e CPF "([^"]*)"$'){
 //When eu tentar cadastrar um político com o nome “Eduardo” e CPF “01234567890”
 //Then o sistema não irá cadastrar o político
 
-Given(~'^o sistema tem um político de nome "([^"]*)" e CPF "([^"]*)"$'){
-    String politicoName, cpf ->
-    Politico politico = Politico.findByCpfAndName(cpf,politicoName)
-    assert politico != null
+Given(~'^que o sistema tem um politico de CPF "([^"]*)"$'){
+    String cpf ->
+        TestDataAndOperations.createPolitico(cpf)
+        Politico politico = Politico.findByCpf(cpf)
+        assert politico != null
 }
 
-When (~'^ao tentar cadastrar um político com o nome "([^"]*)" e CPF "([^"]*)"$'){
-    String politicoName, cpf ->                        //tem que verificar
-    TestDataAndOperations.createPolitico(politicoName,cpf)//tem que verificar
-}
-
-Then(~'^o sistema não irá cadastrar o político de nome "([^"]*)" e CPF "([^"]*)"$') {
-    String politicoName,cpf ->
-    politicos = Politico.findAllByCpfAndName(cpf,politicoName)
-    assert politicos.size() == 1
+Then(~'^o sistema nao ira cadastrar o politico de CPF "([^"]*)"$'){
+    String cpf ->
+        politicos = Politico.findAllByCpf(cpf)
+        assert politicos.size() == 1
 }
--- test/functional/steps/TestDataAndOperations.groovy
+++ test/functional/steps/TestDataAndOperations.groovy
@@ -1,43 +1,37 @@
 package steps
 
 import obraspublicas.*
-import util.*
+
+import util.EnderecoController
 
 class TestDataAndOperations {
 
     static obras = [
-            [nome              : "Praça do Arsenal", descricao: "Praça localizada no recife antigo",
-             imagem            : "http://www.turismonorecife.com.br/sites/default/files/praca_do_arsenal_0.jpg",
-             precoPlanejado    : 1250000.23, precoFinal: 25000000.50, dataPlanejada: (new Date("12 October 2012")),
-             dataTermino       : (new Date("12 October 2016")), latitude: 12, longitude: 45,
-             empresaResponsavel: "Moura Dubeux",
-             politicoResponsavel: TestDataAndOperations.findPoliticoByCPF("01234567891")],
+            [nome               : "Praca do arsenal",
+             descricao          : "Praça localizada no recife antigo",
+             imagem             : "http://www.turismonorecife.com.br/sites/default/files/praca_do_arsenal_0.jpg",
+             precoPlanejado     : 1250000.23,
+             precoFinal         : 25000000.50,
+             dataPlanejada      : (new Date("12 October 2012")),
+             dataTermino        : (new Date("12 October 2016")),
+             latitude           : 12,
+             longitude          : 45,
+             empresaResponsavel : "Moura Dubeux"//,
+             //politicoResponsavel: TestDataAndOperations.findPoliticoByCPF("01234567891")
+            ],
 
-            [nome              : "Ilha do Retiro", descricao: "Casa do leão",
-             imagem            : "youtube.com.br",
-             precoPlanejado    : 1250000.23, precoFinal: 1200000.50, dataPlanejada: (new Date("12 October 2017")),
-             dataTermino       : (new Date("12 October 2010")), latitude: 12, longitude: 45,
-             empresaResponsavel: "Edu Dubeux",
-             politicoResponsavel: TestDataAndOperations.findPoliticoByCPF("98765432109")]
-    ]
-
-
-    static politicos = [
-            [partido  : Partido.SD,
-             foto     : "youtube.com",
-             descricao: "Esse é virado no mói de coentro",
-             qualidade: 10.0,
-             nome     : "Eduardo Lokão",
-             cpf      : "01234567891",
-             email    : "eduobra@obra.com"],
-
-            [partido  : Partido.PSOL,
-             foto     : "youtube.com",
-             descricao: "Esse é LADRÃO",
-             qualidade: 0.0,
-             nome     : "Robson nada Lokão",
-             cpf      : "98765432109",
-             email    : "robsobra@obra.com"]
+            [nome               : "Ilha do Retiro",
+             descricao          : "Casa do leão",
+             imagem             : "youtube.com.br",
+             precoPlanejado     : 1250000.23,
+             precoFinal         : 1200000.50,
+             dataPlanejada      : (new Date("12 October 2017")),
+             dataTermino        : (new Date("12 October 2010")),
+             latitude           : 12,
+             longitude          : 40,
+             empresaResponsavel : "Edu Dubeux"//,
+             //politicoResponsavel: TestDataAndOperations.findPoliticoByCPF("98765432109")
+            ]
     ]
 
     static enderecos = [
@@ -54,6 +48,24 @@ class TestDataAndOperations {
              cidade: "Recife",
              estado: "Pernambuco",
              CEP: "12345-67"]
+    ]
+
+    static politicos = [
+            [partido  : "Partido Pernambucano",
+             foto     : "youtube.com",
+             descricao: "Esse é virado no mói de coentro",
+             qualidade: 10.0,
+             nome     : "Eduardo Lokão",
+             cpf      : "01234567891",
+             email    : "eduobra@obra.com"],
+
+            [partido  : "PT",
+             foto     : "youtube.com",
+             descricao: "Esse é LADRÃO",
+             qualidade: 0.0,
+             nome     : "Robson nada Lokão",
+             cpf      : "98765432109",
+             email    : "robsobra@obra.com"]
     ]
 
     static public def findObraByNome(String obraNome) {
@@ -76,44 +88,46 @@ class TestDataAndOperations {
 
     static public void createObra(String obraNome) {
         def cont = new ObraController()
-        cont.params << TestDataAndOperations.findObraByNome(obraNome)
+        cont.params << TestDataAndOperations.findObraByNome(obraNome) << [politicoResponsavel: TestDataAndOperations.findPoliticoByCPF("01234567891")]
         cont.create()
         cont.save()
         cont.response.reset()
     }
 
-    static public void createPolitico(String politicoCPF) {
+    static public void createPolitico(String cpf) {
         def cont = new PoliticoController()
-        cont.params << TestDataAndOperations.findPoliticoByCPF(politicoCPF)
+        cont.params << TestDataAndOperations.findPoliticoByCPF(cpf)
+        Politico politico = TestDataAndOperations.findPoliticoByCPF(cpf)
         cont.create()
-        cont.save()
+        cont.save(politico)
         cont.response.reset()
     }
 
     static public void createEndereco(String CEP, int numero) {
         def cont = new EnderecoController()
-        cont.params << TestDataAndOperations.findEnderecoByceoAndNumero(CEP, numero)
+        cont.params << TestDataAndOperations.findEnderecoByCEPAndNumero(CEP, numero)
         cont.create()
         cont.save()
         cont.response.reset()
     }
 
-    static public boolean obraCompatibleTo(obra, nomeObra) {
-        def testObra = findObraByName(nomeObra)
+    static public boolean obraCompatibleTo(politico, nomePolitico, cpf) {
+        def testPolitico = TestDataAndOperations.findPoliticoByCPF(cpf)
         def compatible = false
-        if (testObra == null && obra == null) {
+        if (testPolitico == null && politico == null) {
             compatible = true
-        } else if (testObra != null && obra != null) {
+        } else if (testPolitico != null && politico != null) {
             compatible = true
-            testObra.each { key, data ->
-                compatible = compatible && (obra."$key" == data)
+            testPolitico.each { key, data ->
+                compatible = compatible && (politico."$key" == data)
             }
         }
+
         return compatible
     }
 
     static public boolean politicoCompatibleTo(politico, cpf) {
-        def testPolitico = findObraByCPF(cpf)
+        def testPolitico = findPoliticoByCPF(cpf)
         def compatible = false
         if (testPolitico == null && politico == null) {
             compatible = true
